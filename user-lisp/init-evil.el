216c6b301a27ef9cc777f52cf630f9d6ce992c89
Anthony <125139205@qq.com>
2021-09-01 12:24:10 +0800

    switch to sis (fcitx obselete)

--- user-lisp/init-evil.el
+++ user-lisp/init-evil.el
@@ -126,6 +126,32 @@
   :init
   (global-evil-surround-mode 1))
 
-(require 'fcitx)
+(use-package sis
+  ;; :hook
+  ;; enable the /follow context/ and /inline region/ mode for specific buffers
+  ;; (((text-mode prog-mode) . sis-context-mode)
+  ;;  ((text-mode prog-mode) . sis-inline-mode))
+
+  :config
+  ;; For MacOS
+  (sis-ism-lazyman-config
+
+   ;; English input source may be: "ABC", "US" or another one.
+   ;; "com.apple.keylayout.ABC"
+   "com.apple.keylayout.US"
+
+   ;; Other language input source: "rime", "sogou" or another one.
+   ;; "im.rime.inputmethod.Squirrel.Rime"
+   "com.apple.keylayout.pinyin.simplifed")
+
+  ;; enable the /cursor color/ mode
+  (sis-global-cursor-color-mode t)
+  ;; enable the /respect/ mode
+  (sis-global-respect-mode t)
+  ;; enable the /context/ mode for all buffers
+  (sis-global-context-mode t)
+  ;; enable the /inline english/ mode for all buffers
+  (sis-global-inline-mode t)
+  )
 
 (provide 'init-evil)
