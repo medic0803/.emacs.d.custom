48ef5b90255c61d59cbf7649db4e039d624794dc
medic0803 <medicwrf@gmail.com>
2021-10-31 21:16:47 +0000

    add python dev support

--- user-lisp/init-dev.el
+++ user-lisp/init-dev.el
@@ -3,6 +3,12 @@
 (setq-default python-shell-interpreter "/Users/anthony/opt/anaconda3/bin/python3")
 (require' dap-python)
 
+;; require npm, use your system's package manager to install it
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-pyright)
+                         (lsp))))  ; or lsp-deferred
 ;; c/c++
 (require 'dap-lldb)
 (require 'dap-cpptools)
