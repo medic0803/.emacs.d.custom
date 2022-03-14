79bb8637fc0876d7801c8f75a410fed125bb9ba8
Runfeng Wang <medicwrf@gmail.com>
2022-03-14 15:38:39 +0000

    add pdf export support

--- user-lisp/init-blog.el
+++ user-lisp/init-blog.el
@@ -35,4 +35,10 @@
       (insert (format "[[%s%s]]" prefix imagename))
     (insert (format "![%s](%s%s)" imagename prefix imagename))))
 
+;; http://0x100.club/wiki_emacs/mac-org-mode-output-pdf.html
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/texlive/2021/bin/universal-darwin/"))
+(setq exec-path (append exec-path '("/usr/local/texlive/2021/bin/universal-darwin/")))
+
+(setq org-latex-pdf-process '("xelatex -interaction nonstopmode %f" "xelatex -interaction nonstopmode %f"))
+
 (provide 'init-blog)
